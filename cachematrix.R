4f768c382a4a63d1aa54b85a83e8ecb5af440872
HennessyAndrew <87822366+HennessyAndrew@users.noreply.github.com>
2021-08-17 10:19:17 -0400

    Update cachematrix.R
    updating confusing code

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,37 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+  
+  # Took original code, changed 'm' to 'struggle', and "mean" to "solve"
+  makeCacheMatrix <- function(x = matrix()) {
+    struggle <- NULL
+    set <- function(y) {
+      # will have a 2nd function written inside of makeCacheMatrix with paramters
+      x <<- y
+      struggle <<- NULL
+    }
+    get <- function() x
+    # get/create the value of the matrix
+    setsolve <- function(solve) struggle <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+    
+  }
 
-}
+  ##Took original code, changed 'm' to 'i', and "mean" to "solve"
+  cacheSolve <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    struggle <- x$getsolve()
+    if(!is.null(s)) {
+      message("getting cached data")
+      return(struggle)
+    }
+    data <- x$get()
+    struggle <- solve(data, ...)
+    x$setsolve(struggle)
+    s
+  }
+  
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
